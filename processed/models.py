1c42debe4ccb3eccdd3fe2663d9f0b534dc23b60
zhenxi-ee <3263169704@qq.com>
2019-07-13 17:03:25 +0800

    add page model to save clicked page html

--- processed/models.py
+++ processed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from mongoengine import StringField, DateTimeField
 # Create your models here.
+
+
+class DetailPage(models.Model):
+    url = StringField(unique=True, required=True)
+    time = DateTimeField(required=True)
+    html = StringField(required=True)
+    
+    
--- processed/views.py
+++ processed/views.py
@@ -1,13 +1,36 @@
+import datetime
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 # Create your views here.
 from CxExtractor import CxExtractor
 
+from .models import DetailPage
+from mongoengine.queryset import DoesNotExist
+
+def get_saved_page(url):
+    try:
+        detailPage = DetailPage.objects.get(url=url)
+    except DoesNotExist as e:
+        return 1, None
+    else:
+        return True, detailPage
+
 
 def index(request):
     actual_url = request.get_full_path()[request.get_full_path().index('index/') + 6:]
-    print actual_url
-    cx = CxExtractor(threshold=186)
-    html = cx.getHtml(actual_url)
+    flag, detailPage = get_saved_page(actual_url)
+    if flag:
+        html = detailPage.html
+    else:
+        cx = CxExtractor(threshold=186)
+        html = cx.getHtml(actual_url)
+
+        detailPage = DetailPage()
+        detailPage.html = html
+        detailPage.url = actual_url
+        detailPage.time = datetime.datetime.now()
+        detailPage.save()
+
     return HttpResponse(html)
